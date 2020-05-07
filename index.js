448c8903b5eb27043777368c2e4ed1f40913a38f
lmonty22 <lindsay.montgomery@olympian.org>
2020-05-06 20:23:13 -0400

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,77 @@
  for you to use if you need it!
  */
 
+// createEmployeeRecord
+// Argument(s)
+// A 4-element Array of a String, String, String, and Number corresponding to a first name, family name, title, and pay rate per hour
+// Returns
+// JavaScript Object with keys:
+// firstName
+// familyName
+// title
+// payPerHour
+// timeInEvents
+// timeOutEvents
+// Behavior
+// Loads Array elements into corresponding Object properties. Additionally, initialize empty Arrays on the properties timeInEvents and timeOutEvents.
+
+let createEmployeeRecord = function(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+
+let createEmployeeRecords = function(arrayOfArrays) {
+    return arrayOfArrays.map(array => createEmployeeRecord(array))
+}
+
+let createTimeInEvent = function(date) {
+    const dateArray = date.split(" ")
+    const dateObj = {
+        type: "TimeIn",
+        hour: parseInt(dateArray[1]),
+        date: dateArray[0]
+    }
+    this.timeInEvents.push(dateObj)
+    return this 
+}
+
+
+let createTimeOutEvent = function(date){
+    const dateArray = date.split(" ")
+    const timeOutObj = {
+        type: "TimeOut",
+        hour: parseInt(dateArray[1]),
+        date: dateArray[0]
+    }
+    this.timeOutEvents.push(timeOutObj)
+    return this 
+}
+
+let hoursWorkedOnDate = function(date){
+    const timeIn = this.timeInEvents.find(timeIn => timeIn.date === date)
+    const timeOut = this.timeOutEvents.find(timeOut => timeOut.date === date)
+    return (timeOut.hour - timeIn.hour)/100
+}
+
+// Argument(s)
+// A date of the form "YYYY-MM-DD"
+// Returns
+// Pay owed
+// Behavior
+// Using hoursWorkedOnDate, multiply the hours by the record's payRate to determine amount owed. Amount should be returned as a number.
+// let wagesEarnedOnDate = function(){
+
+let wagesEarnedOnDate = function(date){
+    return hoursWorkedOnDate.call(this, date)*this.payPerHour
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +90,23 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+
+let findEmployeeByFirstName = function(array, firstName){
+    return array.find(emp => emp.firstName === firstName)
+}
+// calculatePayroll
+// Argument(s)
+// Array of employee records
+// Returns
+// Pay owed for all dates
+// Behavior
+// Using wagesEarnedOnDate, accumulate the value of all dates worked by the employee in the record used as context. Amount should be returned as a number.
+
+let calculatePayroll = function(array){
+    return array.reduce((sum, emp) => {
+       return  sum + allWagesFor.call(emp)
+    }, 0)
+
 }
